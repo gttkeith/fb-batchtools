03281191771e0261bd3e133ed7ace22343b06a9e
gttkeith <n7lb9k@gmail.com>
2015-08-28 13:23:58 +0800

    whoopsies

--- LikeAllComments.py
+++ LikeAllComments.py
@@ -9,12 +9,17 @@ btoolsfile.import_ids_txt()
 
 print "\nCommenting..."
 for object_id in btoolsfile.targetids:
-    fbio.fb_interact(fbio.put_like,object_id)
+    comments_count=0
+    comments_data=fbio.fb_interact(fbio.get_comments,object_id)
+    for comment in comments_data:
+        comment_id=comment.get("id")
+        fbio.fb_interact(fbio.put_like,comment_id)
+        comments_count+=1
     fbio.remove_from_workingids(object_id)
     if len(fbio.workingids) > 1:
-        print "%s IDs remaining."%len(fbio.workingids)
+        print "%s comment(s) liked; %s IDs remaining."%(comments_count,len(fbio.workingids))
     elif len(fbio.workingids) > 0:
-        print "%s ID remaining."%len(fbio.workingids)
+        print "%s comment(s) liked; %s ID remaining."%(comments_count,len(fbio.workingids))
     
 print "Comments liked!"
 cm.keypress_exit("")
