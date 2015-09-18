fdd227044d014fe3c0abb6d6c78285fb5de7fd62
gttkeith <n7lb9k@gmail.com>
2015-09-18 11:09:23 +0800

    WHOOPS SUPERFLOUS

--- CommentFetcher.py
+++ CommentFetcher.py
@@ -7,16 +7,14 @@ fbauth.begin()
 
 btoolsfile.import_ids_txt()
 
-try:
-    if cm.active_dir==cm.resume_dir:
-        print "Resuming export..."
-        btoolsfile.active_file_obj=open("%s/fb-comments.csv"%cm.export_dir,'a')
-    else:
-        print "Exporting to file..."
-        btoolsfile.active_file_obj=open("%s/fb-comments.csv"%cm.export_dir,'w')
-        btoolsfile.csv_write_line("Parent Post","Parent ID","Created","Name","User ID","Comment","Comment ID")
-except:
-    cm.keypress_exit_traceback()
+
+if cm.active_dir==cm.resume_dir:
+    print "Resuming export..."
+    btoolsfile.active_file_obj=open("%s/fb-comments.csv"%cm.export_dir,'a')
+else:
+    print "Exporting to file..."
+    btoolsfile.active_file_obj=open("%s/fb-comments.csv"%cm.export_dir,'w')
+    btoolsfile.csv_write_line("Parent Post","Parent ID","Created","Name","User ID","Comment","Comment ID")
 
 for post_id in btoolsfile.targetids:
     comment_count=0
